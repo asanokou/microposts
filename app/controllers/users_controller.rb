f22439067a5ab4eb5f8d3ee1300fbc507d502008
asanokou <kou.3.0301@icloud.com>
2017-01-03 15:56:14 +0000

    create user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -10,17 +11,41 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.save # saveにバリデーションで失敗すると、falseを返す。そのとき、
+                  # @user.errorsに値が入る。
       flash[:success] = "Welcom to the sample App!"
       redirect_to @user
+      # @userみたいなActiveRecord::BaseのインスタンスがURLヘルパーで渡される場合、
+      # showアクションに該当するURLヘルパーが使われる。
+      # redirect_to user_path(@user) と同じ。
+      # link_to 'ユーザーの詳細画面へ', @user
+      # 一度UsersControllerのインスタンスを破棄して、アクセスをし直す。
+      # @~~~~はインスタンス変数なので、@~~~も一緒になくなる。
     else
       render 'new'
+      # ビューのレンダリングをして、ブラウザに結果を返す
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to @user , notice: 'プロフィールを変更しました'
+    else
+      render 'edit'
     end
   end
   
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile, :location)
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
 end
